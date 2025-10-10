16ab5451ca46d51b91a91cd3bee11e3311d9693e
latercomer <latercomer@qq.com>
2025-10-10 10:28:14 +0800

    修改了vtol50kg_airframe_init.m中初始化顺序

--- code/airframe/vtol-50kg/vtol50kg_airframe_init.m
+++ code/airframe/vtol-50kg/vtol50kg_airframe_init.m
@@ -19,6 +19,10 @@ mass = init_weight;
 payload = init_payload;
 
 
+%% 物理环境
+environ.wind.const_wind = [0 0 0];
+environ.terrain.hground = 0.0;
+
 
 %% 初始状态
 % 初始速度，体轴系，单位m/s
@@ -28,12 +32,9 @@ nosave_init.p_q_r = [0 0 0];
 % 初始姿态角，但是rad
 nosave_init.phi_theta_psi = [0 0 0];
 % 初始经纬高，单位deg，m
-nosave_init.lat_lon_alt = [30 120 0];
+nosave_init.lat_lon_alt = [30 120 environ.terrain.hground];
 init = Simulink.Parameter(nosave_init);
 
-%% 物理环境
-environ.wind.const_wind = [0 0 0];
-environ.terrain.hground = 0;
 
 % ^(?!nosave_).+ 表示不以 nosave_ 开头的变量
 save vtol50kg_airframe_data.mat -regexp ^(?!nosave_).+

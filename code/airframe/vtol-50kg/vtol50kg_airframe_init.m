78c9e1f9ad1c0606e861dfb816d2b80fc767cf29
latercomer <latercomer@qq.com>
2025-10-11 16:08:09 +0800

    解决motor转向定义错误，优化代码生成接口
    Signed-off-by: latercomer <latercomer@qq.com>

--- code/airframe/vtol-50kg/vtol50kg_airframe_init.m
+++ code/airframe/vtol-50kg/vtol50kg_airframe_init.m
@@ -20,8 +20,8 @@ payload = init_payload;
 
 
 %% 物理环境
-environ.wind.const_wind = [0 0 0];
-environ.terrain.hground = 0.0;
+wind.windspeed = [0 0 0];
+terrain.hground = 0.0;
 
 
 %% 初始状态
@@ -32,7 +32,7 @@ nosave_init.p_q_r = [0 0 0];
 % 初始姿态角，但是rad
 nosave_init.phi_theta_psi = [0 0 0];
 % 初始经纬高，单位deg，m
-nosave_init.lat_lon_alt = [30 120 environ.terrain.hground];
+nosave_init.lat_lon_alt = [30 120 terrain.hground];
 init = Simulink.Parameter(nosave_init);
 
 

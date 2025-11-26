25c62bb0f3815e8bcad81842a4053fb2eace59eb
latercomer <latercomer@qq.com>
2025-11-26 15:39:32 +0800

    起落架模型基本可以起飞了

--- tools/nextpilot_format_mcode.m
+++ tools/nextpilot_format_mcode.m
@@ -1,9 +1,11 @@
+function nextpilot_format_mcode()
+
 cwd = fileparts(mfilename('fullpath')+".m");
 mbeautifier_dir = fullfile(cwd,  'MBeautifier');
 addpath(mbeautifier_dir)
 
 % format m-file in code folder
-root = fullfile(cwd, '../');
+root = fullfile(cwd, '/');
 list = dir(fullfile(root, '**', '*.m'));
 for i = 1:length(list)
     if ~contains(list(i).folder, 'MBeautifier')
--- tools/nextpilot_project_startup.m
+++ tools/nextpilot_project_startup.m
@@ -1,6 +1,14 @@
 function nextpilot_project_startup()
 
-rootpath = slproject.getCurrentProject().RootFolder;
+% 可以通过Simulink.fileGenControl控制simulink缓存和代码生成文件路径
+%
+% CacheFolder - 指定用于仿真的模型编译工件（包括 Simulink®缓存文件）的根文件夹。
+% CodeGenFolder - 指定代码生成文件的根文件夹。
+% CodeGenFolderStructure - 控制代码生成文件夹中的文件夹结构。
+
+% rootpath = slproject.getCurrentProject().RootFolder;
+% rootpath = matlab.project.rootProject().RootFolder;
+rootpath = currentProject().RootFolder;
 
 % 读取机架数据
 % nextpilot_airframe_init();

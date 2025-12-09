5eb1e108de078414645a51914a16390347613962
latercomer <latercomer@qq.com>
2025-12-09 09:35:34 +0800

    优化NAVIGATOR模型

---
+++ code/controller/navigator/navigator_model_data.m
@@ -0,0 +1,9 @@
+function navigator_model_data()
+
+
+% PARAM_DEFINE_CONST('ORIGIN_MAVLINK', uint8(0));
+% PARAM_DEFINE_CONST('ORIGIN_ONBOARD', uint8(1));
+% MISSION_ORIGIN_MAVLINK = uint8(0);
+% MISSION_ORIGIN_ONBOARD = uint8(1);
+
+save navigator_data.mat -regexp ^(?!nosave_).+
